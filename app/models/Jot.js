6713a44d9cb948c3b41158a259ef26135fff48c0
William McElroy <william.j.mcelroy.wm@gmail.com>
2023-08-29 08:59:02 -0600

    drawing date on active jot

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -7,7 +7,7 @@ export class Jot {
     constructor(data) {
         this.id = generateId()
         this.name = data.name
-        this.createdDate = new Date().toDateString() || data.createdDate
+        this.createdDate = new Date().toLocaleDateString() || data.createdDate
         this.reportedDate = data.reportedDate ? new Date(data.reportedDate) : new Date()
         this.reportBody = data.reportBody || ". . . . . "
         this.color = data.color
@@ -23,6 +23,7 @@ export class Jot {
     get ActiveJotTemplate() {
         return `<form onsubmit="app.JotsController.saveJots()">
     <h2 class="text-white"><i class="mdi mdi-note" style="color:${this.color};"></i>${this.name}</h2>
+    <h3 class="text-white">Last Updated:${this.reportedDate.toDateString()} ${this.reportedDate.toLocaleTimeString()}</h3>
     <textarea class="w-100" name="reportBodyContent" id="reportBodyContent" >${this.reportBody}</textarea>
     <button>Save Jot</button>
     </form>`
